40dd3c95b407ea04d6d6e9fd3b3468d36cccc8ea
SemGrg <kdog2105@mail.ru>
2020-09-04 11:16:00 +0300

    Mnogo vsego

--- app/src/main/java/com/example/vorony/MainActivity.java
+++ app/src/main/java/com/example/vorony/MainActivity.java
@@ -2,19 +2,28 @@ package com.example.vorony;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Calendar;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
 
     int counter = 0;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
 
 
         LinearLayout ll = new LinearLayout(this);
@@ -23,19 +32,62 @@ public class MainActivity extends AppCompatActivity {
         hi.setText("Say hello to my little friend!");
         Button pluser = new Button(this);
         pluser.setText("Count plus one");
+        Button minuser = new Button(this);
+        minuser.setText("Count minus one");
+
+        final EditText ed = new EditText(this);
+        ed.setHint("Bведите Ваше имя");
+
+        Button insert = new Button(this);
+        insert.setText("Поздороваться");
+
+        Button sub = new Button(this);
+        sub.setText("Нажми на меня");
+
+        TimePicker mTimePicker = new TimePicker(this);
+
+        Calendar now = Calendar.getInstance();
+
+        mTimePicker.setCurrentHour(now.get(Calendar.HOUR_OF_DAY));
+        mTimePicker.setCurrentMinute(now.get(Calendar.MINUTE));
+
+
         ll.setOrientation(LinearLayout.VERTICAL);
         LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
         params.gravity = Gravity.CENTER;
         a.setLayoutParams(params);
         hi.setLayoutParams(params);
         pluser.setLayoutParams(params);
-        ll.addView(a);
-        ll.addView(hi);
-        ll.addView(pluser);
-    setContentView(ll);
+        minuser.setLayoutParams(params);
+        sub.setLayoutParams(params);
+       ll.addView(a);
+       ll.addView(hi);
+       ll.addView(pluser);
+       ll.addView(minuser);
+       ll.addView(ed);
+        ll.addView(insert);
+      ll.addView(sub);
+
+        ll.addView(mTimePicker);
+
+        ScrollView sv = new ScrollView(this);
+        sv.addView(ll);
+        setContentView(sv);
+
+        sub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast t = Toast.makeText(getApplicationContext(), "Sub", Toast.LENGTH_LONG);
+                t.setGravity(Gravity.TOP,0,160);
+                t.show();
+            }
+        });
+
+
         hi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 a.setText("*minigan sounds*");
             }
         });
@@ -45,6 +97,23 @@ public class MainActivity extends AppCompatActivity {
                 a.setText("There is " + ++counter);
             }
         });
+        minuser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                a.setText("There is " + --counter);
+            }
+        });
+
+        insert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                a.setText("Здравствуйте, " + ed.getText());
+            }
+        });
+
+
 
     }
+
+
 }
